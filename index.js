42ed6d0afbe4704e9084575afb09a38fa52a6db4
Gerald Gorman <gpgorman158@gmail.com>
2022-01-22 16:38:43 -0500

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Write your code here!
+const main = document.getElementById("main");
+main.remove();
+
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.textContent = 'Gerald is the champion';
+document.body.append(newHeader);
+
+
+
+//newHeader.setAttribute('id', 'victory');
+//newHeader.textContent = 'Gerald is the champion';
+
